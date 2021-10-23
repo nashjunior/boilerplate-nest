af6a40f14d602b5764d2149b2d3400adfcda4cc6
nashjunior <nashjuniorjj@gmail.com>
2021-10-23 14:47:39 -0300

    chore: add jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScriptCaseModule } from 'ScriptCase/script-case';
 import { AuthController } from './auth/auth.controller';
@@ -8,6 +10,18 @@ import UsuariosRepository from './auth/users.repository';
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [ScriptCaseModule, TypeOrmModule.forFeature([UsuariosRepository])],
+  imports: [
+    ScriptCaseModule,
+    TypeOrmModule.forFeature([UsuariosRepository]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async () => ({
+        secretOrPrivateKey: process.env.APP_SECRET,
+        signOptions: {
+          expiresIn: 18000,
+        },
+      }),
+    }),
+  ],
 })
 export class AuthModule {}
