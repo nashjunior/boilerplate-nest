2cf2ec3ec4c56bb0bc78d4a6e3a70efa940e5205
nashjunior <nashjuniorjj@gmail.com>
2021-10-23 12:00:04 -0300

    refactor: user repository should find by matricula or senha

--- src/auth/auth/users.repository.ts
+++ src/auth/auth/users.repository.ts
@@ -1,12 +1,17 @@
 import { EntityRepository, Repository } from 'typeorm';
+import CreateLoginDTO from '../create-login.dto';
 import Usuario from '../usuario.entity';
 
 @EntityRepository(Usuario)
 class UsuariosRepository extends Repository<Usuario> {
-  async findByMatricula(matricula: string): Promise<Usuario | undefined> {
+  async findByMatricula({
+    matricula,
+    senha,
+  }: CreateLoginDTO): Promise<Usuario | undefined> {
     return this.findOne({
       where: {
         usu_codigo: matricula,
+        usu_senha: senha,
       },
     });
   }
